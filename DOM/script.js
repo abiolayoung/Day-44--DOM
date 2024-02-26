4b65bdc1485c1d1a6967824bdd19a50c9b196287
Abiolayoung <abiolayoung229@gmail.com>
2024-02-26 12:54:02 +0100

    update

--- DOM/script.js
+++ DOM/script.js
@@ -4,11 +4,21 @@
 // }
 // window.sayHi()
 
-document.body.style.background = "red";
-setTimeout(()=> document.body.style.background = "", 1000)
+// document.body.style.background = "red";
+// setTimeout(()=> document.body.style.background = "", 1000)
 
 // BOM- Browser Object Model
-alert(location.href)
-if(confirm('Go to wikipedia')){
-    location.href = "https://wikipedia.org"
-}
+// alert(location.href)
+// if(confirm('Go to wikipedia')){
+//     location.href = "https://wikipedia.org"
+// }
+
+// THE DOM TREE
+/* the backbone of any html document is the tags, every html tag is an object, nested tags are the children on the enclosing one
+the text inside a tag is an object as well, all these objects are assesible using javascript, 
+and can be used to modify the page.
+document.body is the object representing the body tag.*/
+
+document.body.style.background = "red";
+setTimeout(()=> document.body.style.background = "", 3000)
+
